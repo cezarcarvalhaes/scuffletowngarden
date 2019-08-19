e17f643c41324f448b39da6dbf83f3b1f9786c36
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2019-08-18 21:48:56 -0400

    header on main page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,72 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Img from 'gatsby-image';
+import Layout from "../components/Layout";
+import Image from "../components/Image";
+import SEO from "../components/Seo";
 
-import Layout from "../components/Layout"
-import Image from "../components/Image"
-import SEO from "../components/Seo"
+const IndexPage = (props) => {
+  const { cowScuffler, cowScufflerSmall } = props.data;
+  return (
+    <Layout>
+      <SEO title="Scuffletown Garden"/>
+      <section id="header" className="d-flex flex-column flex-md-row w-100 pt-md-3 container mb-3">
+        <div className="w-25 d-none d-md-flex">
+          <Img alt="Cow Scuffler" fluid={cowScuffler.childImageSharp.fluid} className="w-100"/>
+        </div>
+        <div className="px-3 d-md-none">
+          <Img alt="Cow Scuffler" fluid={cowScufflerSmall.childImageSharp.fluid} className="w-50 mx-auto"/>
+        </div>
+        <div className="w-100 w-md-75 mx-auto darkGreen d-flex flex-column justify-content-center px-3">
+          <h1 className="text-left text-md-center">Scuffletown Garden</h1>
+          <p className="px-md-5" style={{ fontSize: '1.25em' }}>Nestled in the heart of the fan, Scuffletown Garden brings sustainable and friendly New American Cuisine to the iconic neighborhood without breaking the bank.</p>
+          <div className="d-flex justify-content-center mt-3">
+            <a
+              className="btn btn-lg btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="resy"
+              href="https://resy.com/cities/ric/scuffletown-garden-restaurant-bar"
+            >
+              Find a Table
+            </a>
+          </div>
+        </div>
+      </section>
+      {/* <section id="reservation" className="bg-offwhite my-3 py-5">
+        <h2 className="text-center">Make a Reservation</h2>
+        <small className="text-center my-3">Powered by Resy</small>
+        <div className="d-flex justify-content-center">
+          <a
+            className="btn btn-lg btn-primary"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="resy"
+            href="https://resy.com/cities/ric/scuffletown-garden-restaurant-bar"
+          >
+            Find a Table
+          </a>
+        </div>
+      </section> */}
+    </Layout>
+)};
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="mt-5 pt-5">
-      <h1>Hi people</h1>
-      <h2>Yello</h2>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-    </div>
-  </Layout>
-)
+export default IndexPage;
 
-export default IndexPage
+export const query = graphql`
+  query {
+    cowScuffler: file(relativePath: { regex: "/CowScuffler/" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000, quality: 90) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    cowScufflerSmall: file(relativePath: { regex: "/CowScuffler-small/" }) {
+      childImageSharp {
+        fluid(maxWidth: 500, quality: 90) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }`;
